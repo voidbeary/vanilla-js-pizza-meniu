5b2eca475b6f513a38b56dafbbae2188f7981648
Justina Andrulionyte <69106997+voidbeary@users.noreply.github.com>
2021-10-19 21:15:59 +0300

    add dummy button and more css

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const formElement = document.querySelector("form");
 formElement.onsubmit = handleSubmit;
 
 const nameInputElement = document.querySelector("#name");
-nameInputElement.onchange = handleNameInputChange;
+nameInputElement.oninput = handleNameInputChange;
 nameInputElement.onblur = handleBlur;
 
 const toppingsInputElement = document.querySelector("#toppings");
-toppingsInputElement.onchange = handleToppingsInputChange;
+toppingsInputElement.oninput = handleToppingsInputChange;
 toppingsInputElement.onblur = handleBlur;
 
 renderPhotos();
--- render.js
+++ render.js
@@ -14,10 +14,14 @@ function createListItem(pizza) {
   const newListItem = document.createElement("li");
   newListItem.classList.add("pizza-list-item");
 
+  const divElement = document.createElement("div");
+  divElement.classList.add("margin-bot-auto");
   const pizzaNameParagraph = document.createElement("h2");
-  const nameText = document.createTextNode(pizza.name + " " + pizza.heat);
+  const nameText = document.createTextNode(pizza.name + " ");
   pizzaNameParagraph.appendChild(nameText);
-  newListItem.appendChild(pizzaNameParagraph);
+  divElement.appendChild(pizzaNameParagraph);
+  divElement.appendChild(renderChiliPeppers(pizza));
+  newListItem.appendChild(divElement);
 
   const pizzaToppingstParagraph = document.createElement("p");
   const toppingsText = document.createTextNode(pizza.toppings.join(", ") + " ");
@@ -36,6 +40,14 @@ function createListItem(pizza) {
     listImageElement.classList.add("pizza-photo");
     newListItem.appendChild(listImageElement);
   }
+
+  const deleteButtonElement = document.createElement("button");
+  deleteButtonElement.setAttribute("type", "button");
+  const buttonText = document.createTextNode("Delete button");
+  deleteButtonElement.appendChild(buttonText);
+  deleteButtonElement.classList.add("delete-button");
+  newListItem.appendChild(deleteButtonElement);
+
   return newListItem;
 }
 function renderPhotos() {
@@ -62,5 +74,31 @@ function renderPhotos() {
   imageElement3.classList.add("radio-photo");
   const labelElement3 = document.querySelector("#photo-label-3");
   labelElement3.appendChild(imageElement3);
+}
+function renderChiliPeppers(pizza) {
+  const spanElement = document.createElement("span");
+
+  if (pizza.heat == 0) {
+    return spanElement;
+  }
+
+  var svgNS = "http://www.w3.org/2000/svg";
+
+  for (let i = 0; i < pizza.heat; i++) {
+    const svgElement = document.createElementNS(svgNS, "svg");
+    svgElement.setAttribute("viewBox", "0 0 512 512");
+    svgElement.setAttribute("width", "100%");
+    svgElement.setAttribute("height", "100%");
+    svgElement.classList.add("chili-svg");
+    const pathElement = document.createElementNS(svgNS, "path");
+    pathElement.setAttribute("fill", "currentColor");
+    pathElement.setAttribute(
+      "d",
+      "M465 134.2c21.46-38.38 19.87-87.17-5.65-123.1c-7.541-10.83-22.31-13.53-33.2-5.938c-10.77 7.578-13.44 22.55-5.896 33.41c14.41 20.76 15.13 47.69 4.098 69.77C407.1 100.1 388 95.1 368 95.1c-36.23 0-68.93 13.83-94.24 35.92L352 165.5V256h90.56l33.53 78.23C498.2 308.9 512 276.2 512 239.1C512 198 493.7 160.6 465 134.2zM320 288V186.6l-52.95-22.69C216.2 241.3 188.5 400 56 400C25.13 400 0 425.1 0 456S25.13 512 56 512c180.3 0 320.1-88.27 389.3-168.5L421.5 288H320z"
+    );
+    svgElement.appendChild(pathElement);
+    spanElement.appendChild(svgElement);
+  }
+  return spanElement;
 }
 export { renderPizzas, renderPizza, renderPhotos };
